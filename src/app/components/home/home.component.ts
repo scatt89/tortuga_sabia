b65cbaf627d338849c70e17f4dc981f2db82a8f3
scatt89 <dasilvagarciam@gmail.com>
2017-01-05 13:26:54 +0100

    Added CSS for home Component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,7 +10,8 @@ import {ImageService} from "../../services/image.service";
 
 @Component({
   selector: 'home',
-  templateUrl: 'home.component.html'
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit{
   advice: Advice;
